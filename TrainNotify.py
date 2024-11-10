dcfa735024c5ab04633ca1ebdac918777c8360bc
Rithvik Katakam <rithvik@Rithviks-MacBook-Pro.local>
2024-11-10 02:38:54 -0500

    First Commit

---
+++ TrainNotify.py
@@ -0,0 +1,32 @@
+import subprocess
+
+def TrainNotify(phone_number, message_text):
+    apple_script = f'''
+    tell application "Messages"
+        set targetService to 1st service whose service type = iMessage
+        set targetBuddy to buddy "{phone_number}" of targetService
+        send "{message_text}" to targetBuddy
+    end tell
+    '''
+    process = subprocess.Popen(['osascript', '-e', apple_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if stderr:
+        print("Error:", stderr.decode())
+    else:
+        print("Message sent successfully!")
+
+
+# Define the target phone number and message
+phone_number = "+17036736383"  # Replace with the recipient's number
+message_text = "Code done running!"
+
+# Send the message
+TrainNotify(phone_number, message_text)
+
+
+
+
+
+
+
+
